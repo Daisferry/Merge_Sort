e13cda1e841ae9473f2697fececf0c23887df444
Daisferry <vlad.daisferry10@gmail.com>
2024-12-05 23:06:23 +0200

    fix

--- merge_sorting.cpp
+++ merge_sorting.cpp
@@ -1,49 +1,12 @@
 #include <iostream>
-
 using namespace std;
-
 
 //prototypes
 void mergeSort(int* unsArr, int sizeArr);
-void merge(int* unsArr, int* subArr_1, int subArrSize_1, int* subArr_2, int subArrSize_2);
 
-
-
-//merging function -> needs closer inspection ; status: unimplemented
-void merge(int* unsArr, int* subArr_1, int subArrSize_1, int* subArr_2, int subArrSize_2) {
-    int i = 0, j = 0, k = 0;
-
-    while (i < subArrSize_1 && j < subArrSize_2) {
-        if (subArr_1[i] <= subArr_2[j]) {
-            unsArr[k] = subArr_1[i];
-            i++;
-        } else {
-            unsArr[k] = subArr_2[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < subArrSize_1) {
-        unsArr[k] = subArr_1[i];
-        i++;
-        k++;
-    }
-
-    while (j < subArrSize_2) {
-        unsArr[k] = subArr_2[j];
-        j++;
-        k++;
-    }
-}
-
-
-
-//will need to be int*() style, but used as void() for simplicity ; status: complete
-void mergeSort(int* unsArr)
+//status: complete
+void mergeSort(int* unsArr, int sizeArr)
 {
-    int sizeArr = 0;
-    
     if (sizeArr <= 1)
     {
         return;
@@ -101,8 +64,6 @@ void mergeSort(int* unsArr)
 int main()
 {
     const int numberofElements = 11;
-    
-    //int* numberList = new int[numberofElements] { 10 , 9 , 5 , 4 , 11 , 6 , 1 , 8 , 3 , 2 , 7 }; 
 
     int numberList[numberofElements] = { 10 , 9 , 5 , 4 , 11 , 6 , 1 , 8 , 3 , 2 , 7 };
 
